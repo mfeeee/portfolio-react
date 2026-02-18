6be163dcbf1b8ce7ba9863454bcff21e33ecbad0
mafe <mfernandarocosta@gmail.com>
2026-02-18 13:35:27 -0300

    feat: add About, Experience, Projects, Contact, and Footer components to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import React, { useEffect } from "react";
-import { Navbar } from "./components/Navbar";
+import React, { useEffect, useState } from "react";
+import { Navbar } from './components/Navbar';
 import { Profile } from './components/Profile';
+import { About } from './components/About';
+import { Experience } from './components/Experience';
+import { Projects } from './components/Projects';
+import { Contact } from './components/Contact';
+import { Footer } from './components/Footer';
 // import './styles.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
 
 function App() {
 	const [count, setCount] = useState(0);
@@ -28,6 +36,11 @@ function App() {
 		<div className="App">
 			<Navbar theme={theme} toggleTheme={toggleTheme} />
 			<Profile theme={theme} />
+			<About theme={theme} />
+			<Experience theme={theme} />
+			<Projects theme={theme} />
+			<Contact theme={theme} />
+			<Footer />
 		</div>
 	);
 }
