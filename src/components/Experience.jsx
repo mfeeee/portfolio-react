85f2b57c9bb54095461aca658989ec971851e8b3
mafe <mfernandarocosta@gmail.com>
2026-02-18 12:42:54 -0300

    feat: add Experience component to showcase skills and technologies

---
+++ src/components/Experience.jsx
@@ -0,0 +1,236 @@
+import React from 'react';
+
+export function Experience({ theme }) {
+    return (
+        <section id="experience">
+            <p className="text_1_section">Explore Minha</p>
+            <h1 className="title">Experiência</h1>
+            <div className="container_experience_details">
+                <div className="container_about">
+                    <div className="container_details">
+                        <h2 className="experience-subtitle">Backend</h2>
+                        <div className="container_articles">
+                            {/* BACKEND */}
+                            <article>
+                                <img src="./assets/blue/c.png" alt="Icone Experiencia" className="icon" />
+                                <div>
+                                    <h3>C</h3>
+                                    <p>Intermediário</p>
+                                </div>
+                            </article>
+
+                            <article>
+                                <img src="./assets/blue/cpp.png" alt="Icone Experiencia" className="icon" />
+                                <div>
+                                    <h3>C++</h3>
+                                    <p>Intermediário</p>
+                                </div>
+                            </article>
+
+                            <article>
+                                <img src="./assets/blue/php.png" alt="Icone Experiencia" className="icon" />
+                                <div>
+                                    <h3>PHP</h3>
+                                    <p>Intermediário</p>
+                                </div>
+                            </article>
+
+                            <article>
+                                <img src="./assets/blue/python.png" alt="Icone Experiencia" className="icon" />
+                                <div>
+                                    <h3>Python</h3>
+                                    <p>Intermediário</p>
+                                </div>
+                            </article>
+
+                            <article>
+                                <img src="./assets/blue/sql.png" alt="Icone Experiencia" className="icon" />
+                                <div>
+                                    <h3>SQL</h3>
+                                    <p>Intermediário</p>
+                                </div>
+                            </article>
+
+                            <article>
+                                <img src="./assets/blue/typescript.png" alt="Icone Experiencia" className="icon" />
+                                <div>
+                                    <h3>TypeScript</h3>
+                                    <p>Intermediário</p>
+                                </div>
+                            </article>
+                            
+                            <article>
+                                <img src="./assets/blue/javascript.png" alt="Icone Experiencia" className="icon" id="javascript" />
+                                <div>
+                                    <h3>JavaScript</h3>
+                                    <p>Intermediário</p>
+                                </div>
+                            </article>
+
+                            <article>
+                                <img src="./assets/blue/dart.png" alt="Icone Experiencia" className="icon" />
+                                <div>
+                                    <h3>Dart</h3>
+                                    <p>Intermediário</p>
+                                </div>
+                            </article>
+
+                            <article>
+                                <img src="./assets/blue/java.png" alt="Icone Experiencia" className="icon" />
+                                <div>
+                                    <h3>Java</h3>
+                                    <p>Básico</p>
+                                </div>
+                            </article>
+
+                        </div>
+                    </div>
+                    <div className="container_details">
+                        <h2 className="experience-subtitle">Frameworks</h2>
+                        <div className="container_articles">
+                            {/* FRAMEWORKS */}
+                            <article>
+                                <img src="./assets/blue/laravel.png" alt="Icone Experiencia" className="icon" />
+                                <div>
+                                    <h3>Laravel</h3>
+                                    <p>Intermediário</p>
+                                </div>
+                            </article>
+                            <article>
+                                <img src="./assets/blue/django.png" alt="Icone Experiencia" className="icon" />
+                                <div>
+                                    <h3>Django</h3>
+                                    <p>Intermediário</p>
+                                </div>
+                            </article>
+
+                            <article>
+                                <img src="./assets/blue/express.png" alt="Icone Experiencia" className="icon" />
+                                <div>
+                                    <h3>Express.js</h3>
+                                    <p>Básico</p>
+                                </div>
+                            </article>
+
+                            <article>
+                                <img src="./assets/blue/nodejs.png" alt="Icone Experiencia" className="icon" />
+                                <div>
+                                    <h3>Node.js</h3>
+                                    <p>Básico</p>
+                                </div>
+                            </article>
+
+                            <article>
+                                <img src="./assets/blue/deno.png" alt="Icone Experiencia" className="icon" />
+                                <div>
+                                    <h3>Deno</h3>
+                                    <p>Básico</p>
+                                </div>
+                            </article>
+
+                            <article>
+                                <img src="./assets/blue/flutter.png" alt="Icone Experiencia" className="icon" />
+                                <div>
+                                    <h3>Flutter</h3>
+                                    <p>Intermediário</p>
+                                </div>
+                            </article>
+                        </div>
+                    </div>
+                    <div className="container_details">
+                        <h2 className="experience-subtitle">Frontend</h2>
+                        {/* FRONTEND */}
+                        <div className="container_articles">
+                            <article>
+                                <img src="./assets/blue/html.png" alt="Icone Experiencia" className="icon" />
+                                <div>
+                                    <h3>HTML</h3>
+                                    <p>Intermediário</p>
+                                </div>
+                            </article>
+                            
+                            <article>
+                                <img src="./assets/blue/css.png" alt="Icone Experiencia" className="icon" />
+                                <div>
+                                    <h3>CSS</h3>
+                                    <p>Intermediário</p>
+                                </div>
+                            </article>
+                            
+                            <article>
+                                <img src="./assets/blue/bootstrap.png" alt="Icone Experiencia" className="icon" />
+                                <div>
+                                    <h3>Bootstrap</h3>
+                                    <p>Intermediário</p>
+                                </div>
+                            </article>
+                            <article>
+                                <img src="./assets/blue/tailwind.png" alt="Icone Experiencia" className="icon" />
+                                <div>
+                                    <h3>Tailwind</h3>
+                                    <p>Intermediário</p>
+                                </div>
+                            </article>
+                            
+                            <article>
+                                <img src="./assets/blue/figma.png" alt="Icone Experiencia" className="icon" />
+                                <div>
+                                    <h3>Figma</h3>
+                                    <p>Básico</p>
+                                </div>
+                            </article>
+                        </div>
+                    </div>
+                    <div className="container_details">
+                        <h2 className="experience-subtitle">DevOps</h2>
+                        <div className="container_articles">
+                            {/* DEVOPS */}
+                            <article>
+                                <img src="./assets/blue/postman.png" alt="Icone Experiencia" className="icon" />
+                                <div>
+                                    <h3>Postman</h3>
+                                    <p>Básico</p>
+                                </div>
+                            </article>
+                            
+                            <article>
+                                <img src="./assets/blue/git.png" alt="Icone Experiencia" className="icon" />
+                                <div>
+                                    <h3>Git</h3>
+                                    <p>Intermediário</p>
+                                </div>
+                            </article>
+                            
+                            <article>
+                                <img src="./assets/blue/mongodb.png" alt="Icone Experiencia" className="icon" />
+                                <div>
+                                    <h3>Mongo.db</h3>
+                                    <p>Básico</p>
+                                </div>
+                            </article>
+                            
+                            <article>
+                                <img src="./assets/blue/docker.png" alt="Icone Experiencia" className="icon" />
+                                <div>
+                                    <h3>Docker</h3>
+                                    <p>Básico</p>
+                                </div>
+                            </article>
+
+                            <article>
+                                <img src="./assets/blue/kubernetes.png" alt="Icone Experiencia" className="icon" />
+                                <div>
+                                    <h3>Kubernetes</h3>
+                                    <p>Básico</p>
+                                </div>
+                            </article>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <img src={theme === 'dark' ? './assets/dark/arrow.png' : './assets/light/arrow.png'} alt="Icone de Flecha" className="icon arrow" onClick={() => window.location.href='#projects'}/>
+            </div>
+        </section>
+    );
+}
