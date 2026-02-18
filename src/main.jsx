04bb898261dbb76e310a0a48da49d3fcfee2b3f6
mafe <mfernandarocosta@gmail.com>
2026-02-18 14:42:53 -0300

    feat: remove unused stylesheets for cleaner codebase

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import './i18n'
-import './styles.css'
-import './mediaqueries.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
