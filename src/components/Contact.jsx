1afe5be30463614bbf005d550be687a6fb756214
mafe <mfernandarocosta@gmail.com>
2026-02-18 13:47:32 -0300

    feat: implement internationalization for Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,24 +1,26 @@
-import React from 'react';
+
+import { useTranslation } from 'react-i18next';
 
 export function Contact({ theme }) {
+    const { t } = useTranslation();
     return (
     <section id="contact">
-        <p className="text_1_section">Fale Comigo</p>
-        <h1 className="title">Contato</h1>
+        <p className="text_1_section">{t('contact.subtitle')}</p>
+        <h1 className="title">{t('contact.title')}</h1>
         <div className="container_contact">
             <div className="container_info">
                 <div className="container_details_info">
                     <div className="contact_info">
                         <img src="./assets/blue/email.png" alt="Icone Email" className="icon contact-icon "/>
-                        <p><a href="mailto:mfernandarocosta@gmail.com" className="contact-link" id="email">mfernandarocosta@gmail.com</a></p>
+                        <p><a href="mailto:mfernandarocosta@gmail.com" className="contact-link" id="email">{t('contact.email')}</a></p>
                     </div>
                     <div className="contact_info">
                         <img src="./assets/blue/whatsapp.png" alt="Icone Whatsapp" className="icon contact-icon"/>
-                        <p><a href="https://wa.link/ykcpo2" className="contact-link">WhatsApp</a></p>
+                        <p><a href="https://wa.link/ykcpo2" className="contact-link">{t('contact.whatsapp')}</a></p>
                     </div>
                     <div className="contact_info">
                         <img src="./assets/blue/linkedin.png" alt="Icone Linkedin" className="icon contact-icon no-theme-switch"/>
-                        <p><a href="https://www.linkedin.com/in/mfernandacs" className="contact-link">LinkedIn</a></p>
+                        <p><a href="https://www.linkedin.com/in/mfernandacs" className="contact-link">{t('contact.linkedin')}</a></p>
                     </div>
                 </div>
             </div>
