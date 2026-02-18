8d6476f38827c8b95e35df2d58623620c1e42f61
mafe <mfernandarocosta@gmail.com>
2026-02-18 13:47:48 -0300

    feat: add internationalization support to Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
+
+import { useTranslation } from 'react-i18next';
 
 export function Footer({ theme }) {
+    const { t } = useTranslation();
     return (
     <footer>
         <nav>
             <div className="nav-links_container">
                 <ul className="links_nav">
-                    <li><a href="#about">Sobre Mim</a></li>
-                    <li><a href="#experience">Experiência</a></li>
-                    <li><a href="#projects">Projetos</a></li>
-                    <li><a href="#contact">Contato</a></li>
+                    <li><a href="#about">{t('nav.about')}</a></li>
+                    <li><a href="#experience">{t('nav.experience')}</a></li>
+                    <li><a href="#projects">{t('nav.projects')}</a></li>
+                    <li><a href="#contact">{t('nav.contact')}</a></li>
                 </ul>
             </div>
         </nav>
-        <p>Copyright &#169 2026 Maria Fernanda. Todos os Direitos Reservados.</p>
+        <p>{t('footer.copyright')}</p>
     </footer>
     );
 }
