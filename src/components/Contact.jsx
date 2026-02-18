3b8125cc965f56b3ccd4b354b28bf3cece720fe3
mafe <mfernandarocosta@gmail.com>
2026-02-18 14:01:00 -0300

    feat: refactor Contact component to use imported assets for improved resource management

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,11 @@
 
+
 import { useTranslation } from 'react-i18next';
+import emailBlue from '../assets/blue/email.png';
+import whatsappBlue from '../assets/blue/whatsapp.png';
+import linkedinBlue from '../assets/blue/linkedin.png';
+import arrowDark from '../assets/dark/arrow.png';
+import arrowLight from '../assets/light/arrow.png';
 
 export function Contact({ theme }) {
     const { t } = useTranslation();
@@ -11,21 +17,21 @@ export function Contact({ theme }) {
             <div className="container_info">
                 <div className="container_details_info">
                     <div className="contact_info">
-                        <img src="./assets/blue/email.png" alt="Icone Email" className="icon contact-icon "/>
+                        <img src={emailBlue} alt="Icone Email" className="icon contact-icon "/>
                         <p><a href="mailto:mfernandarocosta@gmail.com" className="contact-link" id="email">{t('contact.email')}</a></p>
                     </div>
                     <div className="contact_info">
-                        <img src="./assets/blue/whatsapp.png" alt="Icone Whatsapp" className="icon contact-icon"/>
+                        <img src={whatsappBlue} alt="Icone Whatsapp" className="icon contact-icon"/>
                         <p><a href="https://wa.link/ykcpo2" className="contact-link">{t('contact.whatsapp')}</a></p>
                     </div>
                     <div className="contact_info">
-                        <img src="./assets/blue/linkedin.png" alt="Icone Linkedin" className="icon contact-icon no-theme-switch"/>
+                        <img src={linkedinBlue} alt="Icone Linkedin" className="icon contact-icon no-theme-switch"/>
                         <p><a href="https://www.linkedin.com/in/mfernandacs" className="contact-link">{t('contact.linkedin')}</a></p>
                     </div>
                 </div>
             </div>
             <div>
-                <img src={theme === 'dark' ? './assets/dark/arrow.png' : './assets/light/arrow.png'} alt="Icone de Flecha" className="icon arrow" onClick={() => window.location.href = '#desktop-nav'} />
+                <img src={theme === 'dark' ? arrowDark : arrowLight} alt="Icone de Flecha" className="icon arrow" onClick={() => window.location.href = '#desktop-nav'} />
             </div>
         </div>
     </section>            
