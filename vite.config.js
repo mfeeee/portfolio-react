2085b986122a6aadd4ce65d70e25cdd4a76d4b02
mafe <mfernandarocosta@gmail.com>
2026-03-10 13:34:14 -0300

    feat: add test configuration for improved testing setup with jsdom

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/portfolio-react/',
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js'
+  }
 })
